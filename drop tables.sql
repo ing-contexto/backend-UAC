7dc7ee73a72fc60ce6291bb4f0294c866a060716
Gerardo Reyna <gerardoreyna968@gmail.com>
2025-02-19 16:11:30 -0600

    New tables added on drop tables.sql

--- drop tables.sql
+++ drop tables.sql
@@ -2,11 +2,13 @@
 -- DROP TABLE IF EXISTS Municipio_DatosEspecificos;
 -- DROP TABLE IF EXISTS MunicipioColindante;
 
-
 -- DROP TABLE IF EXISTS DatosEspecificos;
--- DROP TABLE IF EXISTS Municipio;
 
--- DROP TABLE IF EXISTS Distrito
--- DROP TABLE IF EXISTS Region
+DROP TABLE IF EXISTS PuntosCardinales;
 
-DROP TABLE IF EXISTS PuntosCardinales
+DROP TABLE IF EXISTS Municipio_GrupoCriminal;
+DROP TABLE IF EXISTS GrupoCriminal;
+DROP TABLE IF EXISTS Localidad;
+DROP TABLE IF EXISTS SectorActividadEconomica;
+DROP TABLE IF EXISTS ActividadEconomica;
+DROP TABLE IF EXISTS Actividad_Economica_Municipio;
